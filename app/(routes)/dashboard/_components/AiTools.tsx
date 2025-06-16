b0ee328ec90a5a1c84a27238ccfabe20f12b747f
Mohammad Zahran <125310252+AbouZik@users.noreply.github.com>
2025-06-16 14:08:34 +0300

    feat: style AiTools component and add descriptive content

--- app/(routes)/dashboard/_components/AiTools.tsx
+++ app/(routes)/dashboard/_components/AiTools.tsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import React from "react";
 
 function AiTools() {
   return (
-    <div>AiTools</div>
-  )
+    <div className="mt-7 p-5 bg-white border rounded-xl">
+      <h2 className="font-bold text-lg">Available AI Tools</h2>
+      <p>Start Building and Shape Your Career with this exclusive AI Tools</p>
+    </div>
+  );
 }
 
-export default AiTools
+export default AiTools;
