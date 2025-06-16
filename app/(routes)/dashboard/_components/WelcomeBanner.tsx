c56f2c1a2693647e9c2b7a53fc17d8c6c61f7dec
Mohammad Zahran <125310252+AbouZik@users.noreply.github.com>
2025-06-16 13:59:48 +0300

    feat: style WelcomeBanner with gradient, content, and CTA button

--- app/(routes)/dashboard/_components/WelcomeBanner.tsx
+++ app/(routes)/dashboard/_components/WelcomeBanner.tsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import { Button } from "@/components/ui/button";
+import React from "react";
 
 function WelcomeBanner() {
   return (
-    <div>WelcomeBanner</div>
-  )
+    <div className="p-5 bg-gradient-to-r from-[#BE575F] via-[#A338E3] to-[#AC76D6] rounded-xl">
+      <h2 className="font-bold text-2xl text-white">AI Career Coach Agent</h2>
+      <p className="text-white">
+        "Smarter career decisions starts here - get tailored advice, real-time
+        market insights, and a roadmap built just for you with the power of AI"
+      </p>
+      <Button variant={"outline"} className="mt-3">
+        Let's Get Started
+      </Button>
+    </div>
+  );
 }
 
-export default WelcomeBanner
+export default WelcomeBanner;
