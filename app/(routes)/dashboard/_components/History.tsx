96033193d527b7bbbb4f4b33076bf2addca2f252
Mohammad Zahran <125310252+AbouZik@users.noreply.github.com>
2025-06-17 16:01:07 +0300

    feat: implement History component with empty state UI

--- app/(routes)/dashboard/_components/History.tsx
+++ app/(routes)/dashboard/_components/History.tsx
@@ -1,9 +1,24 @@
-import React from 'react'
+"use client";
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+import React, { useState } from "react";
 
 function history() {
+  const [userHistory, setUserHistory] = useState([]);
   return (
-    <div>history</div>
-  )
+    <div className="mt-5 p-5 border rounded-xl">
+      <h2 className="font-bold text-lg">Previous History</h2>
+      <p>What Your previously work on, You can find here</p>
+
+      {userHistory?.length == 0 && (
+        <div className="flex items-center justify-center flex-col mt-6">
+          <Image src={"/idea.png"} alt="bulb" width={50} height={50} />
+          <h2>You do Not Have any history</h2>
+          <Button className="mt-5">Explore AI Tools</Button>
+        </div>
+      )}
+    </div>
+  );
 }
 
-export default history
+export default history;
