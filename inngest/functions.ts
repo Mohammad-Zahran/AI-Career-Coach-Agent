206e1baf1186bc248f02f14abc38382b22d8e4b0
Mohammad Zahran <125310252+AbouZik@users.noreply.github.com>
2025-06-18 14:20:11 +0300

    feat: implement AiCareerAgent with system prompt and Gemini-powered response

--- inngest/functions.ts
+++ inngest/functions.ts
@@ -1,4 +1,5 @@
 import { inngest } from "./client";
+import { createAgent, anthropic, openai } from "@inngest/agent-kit";
 
 export const helloWorld = inngest.createFunction(
   { id: "hello-world" },
@@ -6,5 +7,27 @@ export const helloWorld = inngest.createFunction(
   async ({ event, step }) => {
     await step.sleep("wait-a-moment", "1s");
     return { message: `Hello ${event.data.email}!` };
+  }
+);
+
+export const AiCareerChatAgent = createAgent({
+  name: "AiCareerChatAgent",
+  description: "An Ai Agent that answers career related questions",
+  system: `You are a helpful, professional AI Career Coach and your role is to guide users with questions related to careers, including job search advice, interview preparation, resume improvement, skill development, career transitions, and industry trends.
+    Always respond with clarity encouragment, and encouragemet, and actionable advice tailored to the user's needs.
+    If the user asks something unrelated to careers (e.g., topics like health, relationships, coding help, general trivia), gently inform that you are a career coach and suggest relevant career-focused questions instead`,
+  model: openai({
+    model: "gemini-2.0-flash",
+    apiKey: process.env.GEMENI_API_KEY,
+  }),
+});
+
+export const AiCareerAgent = inngest.createFunction(
+  { id: "AiCareerAgent" },
+  { event: "AiCareerAgent" },
+  async ({ event, step }) => {
+    const { userInput } = await event?.data;
+    const result = await AiCareerChatAgent.run(userInput);
+    return result;
   }
 );
