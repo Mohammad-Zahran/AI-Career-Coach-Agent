64ef16484dd68a4c096a4a043a99d28bef6c4dac
Mohammad Zahran <125310252+AbouZik@users.noreply.github.com>
2025-06-18 15:53:01 +0300

    feat: display chat messages with role-based styling and loading spinner

--- app/(routes)/ai-tools/ai-chat/page.tsx
+++ app/(routes)/ai-tools/ai-chat/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
-import { Send } from "lucide-react";
+import { LoaderCircle, Send } from "lucide-react";
 import React, { useEffect, useState } from "react";
 import EmptyState from "./_components/EmptyState";
 import axios from "axios";
@@ -15,7 +15,18 @@ type messages = {
 function page() {
   const [userInput, setUserInput] = useState<string>("");
   const [loading, setLoading] = useState<boolean>(false);
-  const [messageList, setMessageList] = useState<messages[]>([]);
+  const [messageList, setMessageList] = useState<messages[]>([
+    {
+      content: "User Msg",
+      role: "user",
+      type: "text",
+    },
+    {
+      content: "Assistant Msg",
+      role: "assistant",
+      type: "text",
+    },
+  ]);
 
   const onSend = async () => {
     setLoading(true);
@@ -35,6 +46,12 @@ function page() {
     setLoading(false);
   };
 
+  console.log(messageList);
+
+  useEffect(() => {
+    // Save message to Database
+  }, [messageList]);
+
   return (
     <div className="px-10 md:px-24 lg:px-36 xl:px-48">
       <div className="flex items-center justify-between gap-8">
@@ -48,14 +65,43 @@ function page() {
         <Button>+ New Chat</Button>
       </div>
       <div className="flex flex-col h-[75vh]">
-        <div className="mt-5">
-          {/* Empty State options */}
-          <EmptyState
-            selectedQuestion={(question: string) => setUserInput(question)}
-          />
-        </div>
+        {messageList?.length <= 0 && (
+          <div className="mt-5">
+            {/* Empty State options */}
+            <EmptyState
+              selectedQuestion={(question: string) => setUserInput(question)}
+            />
+          </div>
+        )}
 
-        <div className="flex-1">{/* Message List */}</div>
+        <div className="flex-1">
+          {/* Message List */}
+          {messageList?.map((message, index) => (
+            <div>
+              <div
+                key={index}
+                className={`flex mb-2 ${
+                  message.role === "user" ? "justify-end" : "justify-start"
+                }`}
+              >
+                <div
+                  className={`p-3 rounded-lg gap-2 ${
+                    message.role === "user"
+                      ? "bg-gray-200 text-black rounded-lg"
+                      : "bg-gray-50 text-black"
+                  } mb-2`}
+                >
+                  {message.content}
+                </div>
+              </div>
+              {loading && messageList?.length - 1 == index && (
+                <div className="flex justify-start p-3 rounded-lg gap-2 bg-gray-50 text-black mb-2">
+                  <LoaderCircle className="animate-spin" />
+                </div>
+              )}
+            </div>
+          ))}
+        </div>
 
         <div className="flex justify-between items-center gap-6">
           {/* Input Field */}
