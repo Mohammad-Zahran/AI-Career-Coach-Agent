def8f27fbd7c775ff04d2fb6058c435fda54f245
Mohammad Zahran <125310252+AbouZik@users.noreply.github.com>
2025-06-21 02:29:18 +0300

    feat(resume): handle PDF upload, extract text using LangChain, trigger Inngest AiResumeAgent

--- app/api/ai-resume-agent/route.tsx
+++ app/api/ai-resume-agent/route.tsx
@@ -1,6 +1,9 @@
-import { NextRequest } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 import { WebPDFLoader } from "@langchain/community/document_loaders/web/pdf";
+import { inngest } from "@/inngest/client";
+import axios from "axios";
 
+// This API route handles the upload of a resume PDF file, processes it, and returns the extracted text.
 export async function POST(req: NextRequest) {
   const formData = await req.formData();
   const resumeFile: any = formData.get("resumeFile");
@@ -12,4 +15,38 @@ export async function POST(req: NextRequest) {
 
   const arrayBuffer = await resumeFile.arrayBuffer();
   const base64 = Buffer.from(arrayBuffer).toString("base64");
+
+  const resultIds = await inngest.send({
+    name: "AiResumeAgent",
+    data: {
+      userInput: recordId,
+      resumeFile: resumeFile,
+      base64ResumeFile: base64,
+      pdfText: docs[0]?.pageContent,
+    },
+  });
+  const runId = resultIds?.ids[0];
+
+  let runStatus;
+  while (true) {
+    runStatus = await getRuns(runId);
+    if (runStatus?.data[0]?.status === "Completed") break;
+
+    await new Promise((resolve) => setTimeout(resolve, 500));
+  }
+
+  return NextResponse.json(runStatus.data?.[0]?.output?.output[0]);
+}
+
+export async function getRuns(runId: string) {
+  const result = await axios.get(
+    process.env.INNGEST_SERVER_HOST + "/v1/events/" + runId + "/runs",
+    {
+      headers: {
+        Authorization: `Bearer ${process.env.INNGEST_SIGNING_KEY}`,
+      },
+    }
+  );
+
+  return result.data;
 }
